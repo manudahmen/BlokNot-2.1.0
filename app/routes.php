72fbc8da249a4f3ef56cfc15d59c756c6c1364f1
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-02 10:22:14 +0200

    Try to fix something wrong

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
@@ -13,26 +14,30 @@ Route::get('profile', [
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-
+*/
+/*
 Route::get('note/view/{id}', function($id) { 
-    return View::make('note')->with('id', $id); 
+    return view('note')->with('id', $id); 
 })->where('id', '[0-9]+');
 
 Route::get('note/edit/{id}', function($id) { 
     return view('note/edit')->with('id', $id); 
 })->where('id', '[0-9]+');
-
+*/
+Route::get("/", function()
+{
+    return view("blocnotes");
+});
 Route::get("about", function()
 {
-    return View::make("about");
+    return view("about");
 });
 
-
+/*
 Route::get("home", function ()
 {
 
-    return View::make("home");
+    return view("home");
 	
 });
 
@@ -45,3 +50,7 @@ Route::get("blocnotes", function ()
 {
     return View::make("blocnotes");
 });
+ * 
+ * */
+?>
+ */
