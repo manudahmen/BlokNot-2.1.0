52ef75d4e2612f6c55fc48206df26a7829f422ce
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-03 11:01:03 +0200

    Correction

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,57 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Using A Controller...
+
+Route::get('profile', [
+    'middleware' => 'auth',
+    'uses' => 'ProfileController@show'
+]);
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
+Route::get('note/view/{id}', function($id) { 
+    return view('note')->with('id', $id); 
+})->where('id', '[0-9]+');
+
+Route::get('note/edit/{id}', function($id) { 
+    return view('note/edit')->with('id', $id); 
+})->where('id', '[0-9]+');
+
+
+Route::get("/", function()
+{
+    return view("blocnotes");
+});
+Route::get("about", function()
+{
+    return view("about");
+});
+
+Route::get("home", function ()
+{
+
+    return view("home");
+	
+});
+
+Route::get("freezer", function ()
+{
+    return View::make("freezer");
+});
+
+Route::get("blocnotes", function ()
+{
+    return View::make("blocnotes");
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+?>
