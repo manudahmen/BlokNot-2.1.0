38bd110425597e28ff4be8f9173ad1ae01087cbc
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-11 14:22:30 +0200

    Uses old libs

--- resources/views/note/list.blade.php
+++ resources/views/note/list.blade.php
@@ -5,3 +5,13 @@
  * Date: 11/08/2015
  * Time: 14:12
  */
+require_once(asset("Libs/event/main.php"));
+
+$rootDoc = getRootForUser();
+
+$docs = getDocumentsFiltered("", false);
+
+       while(($doc=mysqli_fetch_assoc($docs))!=NULL)
+           {
+               displayNote($doc["id"]);
+           }
