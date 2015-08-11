5040f48ffb399eefcf002c37068765d444445ef1
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-11 15:24:37 +0200

    Typo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 // Authentication routes...
-Route::get('auth/login', ["as" => "login_form" , "uses" =>'Auth\AuthController@getLogin']);
+Route::get('auth/login', ["as" => "login_form", "uses" => 'Auth\AuthController@getLogin']);
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
@@ -17,38 +17,38 @@ Route::post('auth/register', ['as' => 'register_submit', 'uses' => 'Auth\AuthCon
 
 
 Route::get("note/view/{id}", [
-    'middleware' => "auth",
-    'uses' => "NoteController@display"
-        ]
+        'middleware' => "auth",
+        'uses' => "NoteController@display"
+    ]
 )->where('id', '[0-9]+');
 
-Route::get('note/edit/{id}', ['middleware' => "auth", "uses" => function($id) {
-        return view('note/edit')->with('id', $id);
-    }])->where('id', '[0-9]+');
+Route::get('note/edit/{id}', ['middleware' => "auth", "uses" => function ($id) {
+    return view('note/edit')->with('id', $id);
+}])->where('id', '[0-9]+');
 
 Route::post("note/save", [
-    'middleware' => "auth",
-    'uses' => "NoteController@save"
-        ]
+        'middleware' => "auth",
+        'uses' => "NoteController@save"
+    ]
 );
 Route::post("note/media", [
-    'middleware' => "auth",
-    'uses' => "NoteController@addMedia"
-        ]
+        'middleware' => "auth",
+        'uses' => "NoteController@addMedia"
+    ]
 );
 
 Route::post("note/addData", [
-    'middleware' => "auth",
-    'uses' => "NoteController@addData"
-        ]
+        'middleware' => "auth",
+        'uses' => "NoteController@addData"
+    ]
 );
 
-Route::get("/", ["as" => "root", "uses" => function() {
+Route::get("/", ["as" => "root", "uses" => function () {
         return view("home");
     }
-        ]
+    ]
 );
-Route::get("about", function() {
+Route::get("about", function () {
     return view("about");
 });
 
@@ -62,10 +62,9 @@ Route::get("freezer", function () {
 });
 
 Route::get("notes", ["as" => "notes", "uses" => function () {
-        return View::make("notes");
-    }
+    return View::make("notes");
+}
 ]);
-Route::get("notes/list", function()
-{
-    return View::;make("notes/list");
+Route::get("notes/list", function ($folderId=NULL) {
+    return View::make("notes/list/$folderId");
 });
