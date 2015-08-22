e2bdce332d7e3c808dd4e303379690bd95d7194a
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-22 13:57:45 +0200

    Activate Accordian

--- resources/views/freezer.blade.php
+++ resources/views/freezer.blade.php
@@ -5,7 +5,7 @@
     @parent
     <link href="{{asset("css/freezer.css")}}" type="text/css" rel="stylesheet"/>
     <script type="text/javascript" src="{{asset("scripts/jquery-1.11.2.js")}}"></script>
-    <!--<script type="text/javascript" src="{{asset("scripts/layoutActions.js")}}"></script>-->
+    <script type="text/javascript" src="{{asset("scripts/layoutActions.js")}}"></script>
     <script type="text/javascript" src="{{asset("scripts/play.js")}}"></script>
     <script type="text/javascript" src="{{asset("scripts/auth.js")}}"></script>
     <script type="text/javascript" src="scripts/playlist.js"></script>
@@ -15,7 +15,9 @@
 @section("content")
     <h2>Freezer - Search on Gracenote!</h2>
     <p><a target="_blank" href="https://fr.wikipedia.org/wiki/Compact_Disc_Data_Base">CDDB on Wikipedia</a></p>
-    <blockquote>CDDB est un nom de marque d&eacute;pos&eacute; par la soci&eacute;t&eacute; Gracenote qui fournit un service d'acc&egrave;s &agrave; sa base de donn&eacute;&eacute;es &agrave; diff&eacute;rents partenaires :</blockquote>
+    <blockquote>CDDB est un nom de marque d&eacute;pos&eacute; par la soci&eacute;t&eacute; Gracenote qui fournit un
+        service d'acc&egrave;s &agrave; sa base de donn&eacute;&eacute;es &agrave; diff&eacute;rents partenaires :
+    </blockquote>
     <div id="search-form-container">
         <form method="GET" action="{{ URL::to('freezer') }}">
             <fieldset>
@@ -24,17 +26,20 @@
 
                     <tr>
                         <td><label for="artist">Artiste(s)</label></td>
-                        <td><input type="text" name="artist" id="artist" value="<?php echo filter_input(INPUT_GET, "artist") ?>"/>
+                        <td><input type="text" name="artist" id="artist"
+                                   value="<?php echo filter_input(INPUT_GET, "artist") ?>"/>
                         </td>
                     </tr>
                     <tr>
                         <td><label for="album">Album</label></td>
-                        <td><input type="text" name="album" id="album" value="<?php echo filter_input(INPUT_GET, "album") ?>"/>
+                        <td><input type="text" name="album" id="album"
+                                   value="<?php echo filter_input(INPUT_GET, "album") ?>"/>
                         </td>
                     </tr>
                     <tr>
                         <td><label for="yt">Link to Youtube search</label></td>
-                        <td><input type="checkbox" name="yt" id="yt" value="<?php echo filter_input(INPUT_GET, "yt") ?>"/>
+                        <td><input type="checkbox" name="yt" id="yt"
+                                   value="<?php echo filter_input(INPUT_GET, "yt") ?>"/>
                         </td>
                     </tr>
                     <tr>
@@ -78,96 +83,97 @@
         <a id='authorBioNOLinkHide' onclick='cacherBioArtist("authorBioNO");'>Cacher Bio artiste</a>
 
         <div id='authorBioNO'>
-            <?php if (isset($results[0]["artist_image_url"])&& (strlen($results[0]["artist_image_url"])>0)) { ?>
-        <img src='<?php echo $results[0]["artist_image_url"]; ?>'>
-        <?php }
-            if (isset($results[0]["artist_bio_url"])&& (strlen($results[0]["artist_bio_url"])>0)) {
-            echo file_get_contents($results[0]["artist_bio_url"]);
-        } ?>
+            <?php if (isset($results[0]["artist_image_url"]) && (strlen($results[0]["artist_image_url"]) > 0)) { ?>
+            <img src='<?php echo $results[0]["artist_image_url"]; ?>'>
+            <?php }
+            if (isset($results[0]["artist_bio_url"]) && (strlen($results[0]["artist_bio_url"]) > 0)) {
+                echo file_get_contents($results[0]["artist_bio_url"]);
+            } ?>
         </div>
-    <?php
+        <?php
 
-    }
+        }
 
 
-    $i = 0;
-    $j = 0;
-    while (isset($results[$i])) {
-    if(isset($results[$i]["album_gnid"]))
-    {
-        $result0 = $api->fetchAlbum($results[$i]["album_gnid"]);
-    }
-    else{
-    ?>L'album est introuvable, erreur de script ou de base de donn&eacute;es'<?php
-    continue 1;
-    }
-    if(isset($result0[0]))
-    {
-    ?>
-    <p class="album_title"><a href="#album<?= $i ?>" onclick='montrerAlbum("album<?= $i ?>");'>
-            <?= $result0[0]["album_artist_name"] ?>,
-            <?= $result0[0]["album_title"] ?>-- date : <?= $results[$i]["album_year"] ?>
-        </a></p>
-    <div class='album_view' id="album<?= $i ?>">
+        $i = 0;
+        $j = 0;
+        while (isset($results[$i])) {
+        if(isset($results[$i]["album_gnid"]))
+        {
+            $result0 = $api->fetchAlbum($results[$i]["album_gnid"]);
+        }
+        else{
+        ?>L'album est introuvable, erreur de script ou de base de donn&eacute;es'<?php
+        continue 1;
+        }
+        if(isset($result0[0]))
+        {
+        ?>
         <p class="album_title"><a href="#album<?= $i ?>" onclick='montrerAlbum("album<?= $i ?>");'>
-                <i><?= $result0[0]["album_artist_name"] ?></i>
-                <strong><?= $result0[0]["album_title"] ?></strong><?= $results[$i]["album_year"] ?>
+                <?= $result0[0]["album_artist_name"] ?>,
+                <?= $result0[0]["album_title"] ?>-- date : <?= $results[$i]["album_year"] ?>
             </a></p>
-        <img src='<?= $result0[0]["album_art_url"] ?>'>
 
-        <p class='genre_title'>Pistes</p>
-        <ul>
-            <?php
-            foreach ($result0[0]['tracks'] as $track) {
-            ?>
-            <li><label for='musicSpan<?php echo $j; ?>' ><?php echo $track["track_title"] ?></label>
-                <input type='text' class='musicSpan' id='musicSpan<?php echo $j; ?>'
-                       value="<?php echo $result0[0]["album_artist_name"]; ?> , <?php echo $result0[0]["album_title"]; ?> , <?php echo $track["track_title"]; ?>"/>
-                <input type='button' value='Lire chanson sur Youtube'
-                       onclick="playsong('<?php echo rawurlencode($result0[0]["album_artist_name"] . " " . $result0[0]["album_title"] . " " . $track["track_title"]); ?>');"/>
-            </li>
-            <?php
-            $j++;
-            }
-            ?>
-        </ul>
-        <p class='genre_title'>Genres</p>
-        <ul>
-            <?php
-            foreach ($result0[0]['genre'] as $genre) {
-                echo "<li>" . $genre['text'] . "</li>";
-            }?>
-        </ul>
-        <?php if($results[$i]["review_url"] != "")
-        { ?>
-        <div class='album_review'>
-            <p><a href="<?php echo $results[$i]["review_url"];?>">Album review</a></p>
-            <?php if ($results[$i]["review_url"] != NULL) {
-                echo file_get_contents($results[$i]["review_url"]);
-            } ?></div>
-         </div>
-    <?php
-    }
-    }
-    $i++;
-    }
-    ?>
+        <div class='album_view' id="album<?= $i ?>">
+            <p class="album_title"><a href="#album<?= $i ?>" onclick='montrerAlbum("album<?= $i ?>");'>
+                    <i><?= $result0[0]["album_artist_name"] ?></i>
+                    <strong><?= $result0[0]["album_title"] ?></strong><?= $results[$i]["album_year"] ?>
+                </a></p>
+            <img src='<?= $result0[0]["album_art_url"] ?>'>
+
+            <p class='genre_title'>Pistes</p>
+            <ul>
+                <?php
+                foreach ($result0[0]['tracks'] as $track) {
+                ?>
+                <li><label for='musicSpan<?php echo $j; ?>'><?php echo $track["track_title"] ?></label>
+                    <input type='text' class='musicSpan' id='musicSpan<?php echo $j; ?>'
+                           value="<?php echo $result0[0]["album_artist_name"]; ?> , <?php echo $result0[0]["album_title"]; ?> , <?php echo $track["track_title"]; ?>"/>
+                    <input type='button' value='Lire chanson sur Youtube'
+                           onclick="playsong('<?php echo rawurlencode($result0[0]["album_artist_name"] . " " . $result0[0]["album_title"] . " " . $track["track_title"]); ?>');"/>
+                </li>
+                <?php
+                $j++;
+                }
+                ?>
+            </ul>
+            <p class='genre_title'>Genres</p>
+            <ul>
+                <?php
+                foreach ($result0[0]['genre'] as $genre) {
+                    echo "<li>" . $genre['text'] . "</li>";
+                }?>
+            </ul>
+            <?php if($results[$i]["review_url"] != "")
+            { ?>
+            <div class='album_review'>
+                <p><a href="<?php echo $results[$i]["review_url"];?>">Album review</a></p>
+                <?php if ($results[$i]["review_url"] != NULL) {
+                    echo file_get_contents($results[$i]["review_url"]);
+                } ?></div>
+        </div>
+        <?php
+        }
+        }
+        echo "</div>";
+        $i++;
+        }
+        ?>
 
 
+        <div id="#player"></div>
 
-    <div id="#player"></div>
 
-
-    <div id="media_view">
-        <div id="search-container">Player here</div>
-        <br/><span id='query'></span>
+        <div id="media_view">
+            <div id="search-container">Player here</div>
+            <br/><span id='query'></span>
         </div>
         <div id="results">
         </div>
-    <script type="text/javascript">
-        $("a").addClass("btn btn-large btn-primary openbutton")
-//        mettreEnPageInitiale();
-    </script>
+        <script type="text/javascript">
+            $("a").addClass("btn btn-large btn-primary openbutton")
+            mettreEnPageInitiale();
+        </script>
 
 @stop
 
