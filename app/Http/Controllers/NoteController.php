3d364b72866ff4a04097d3ccb44f26f512908e5b
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-23 16:18:19 +0200

    Continue  file uploads

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -5,23 +5,29 @@ namespace App\Http\Controllers;
 use App\Note;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
+
 require_once(realpath(base_path("public/lib/bloc-notes/all-configured-and-secured-included.php")));
 
-class NoteController extends Controller {
+class NoteController extends Controller
+{
 
-    function display($noteId) {
+    function display($noteId)
+    {
         $doc = mysqli_fetch_assoc(getDBDocument($noteId));
         return view("note/view")->with("note", $doc);
     }
 
-    function edit($noteId) {
+    function edit($noteId)
+    {
         $doc = mysqli_fetch_assoc(getDBDocument($noteId));
         return view("note/edit")->with("note", $doc);
     }
 
-    function index() {
+    function index()
+    {
         return view('actionOnNote');
     }
+
     function newnote()
     {
         return view("note/edit")->with("note", 0);
@@ -31,17 +37,17 @@ class NoteController extends Controller {
     {
         $note = Note::findOrNew($request->get("noteId"));
 
-        $note->setAttribute("id",$request->get("noteId"));
+        $note->setAttribute("id", $request->get("noteId"));
         $note->setAttribute("folder_id", $request->get("folder_id"));
         $note->setAttribute("filename", $request->get("filename"));
         $note->setAttribute("content_file", $request->get("content_file"));
         $note->setAttribute("mime", $request->get("mime"));
-        $note->fillable( ["id" =>$note->noteId,
+        $note->fillable(["id" => $note->noteId,
             "folder_id" => $note->folder_id,
             "filename" => $note->filename,
             "content_file" => $note->content_file,
-            "mime" =>$note->mime
-            ]);
+            "mime" => $note->mime
+        ]);
 
         print_r($note);
 
@@ -50,10 +56,12 @@ class NoteController extends Controller {
         return "Save note txt(TODO)";
 
     }
+
     function saveImg()
     {
         return "Save note img(TODO)";
     }
+
     function saveOther()
     {
         return "Save note else types (no txt, no img) (TODO)";
@@ -61,35 +69,40 @@ class NoteController extends Controller {
 
     function linkFile($noteId)
     {
-        
+
     }
+
     /**
-     * Envoie un fichier morceau par morceau pour éviter les 
+     * Envoie un fichier morceau par morceau pour éviter les
      * limitations mémoire php ou mysql.
-     * 
-     * @param type $fileId 
+     *
+     * @param type $fileId
      * @param type $dataChunk morceau du fichier
      */
     function addData($fileId, $dataChunk)
     {
-        
+
     }
+
     function upload(Request $request)
     {
-        $file = $request->file('file');
-        $mime = $file->getMimeType();
-        $content_file = file_get_contents($file->getPath()."/".$file->getBasename());
-        $filename = $file->getBasename();
+        $files = $request->file('file');
+        foreach ($files as $file) {
+            $mime = $file->getMimeType();
+            $content_file = file_get_contents($file->getPath() . "/" . $file->getBasename());
+            $filename = $file->getBasename();
 
-        $note = Note::findOrNew(0);
-        $note->setAttribute("mime", $mime);
-        $note->setAttribute("content_file", $content_file);
-        $note->setAttribute("filename", $filename);
+            $note = Note::findOrNew(0);
+            $note->setAttribute("mime", $mime);
+            $note->setAttribute("content_file", $content_file);
+            $note->setAttribute("filename", $filename);
 
-        $note->save();
+            $note->save();
 
 
-        return "Note saved and uploaded";
+            return "Note saved and uploaded";
+        }
     }
 }
+
 ?>
