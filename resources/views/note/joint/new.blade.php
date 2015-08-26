6de74385e566a19574679d7eb081bbdb375eb048
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-26 03:02:36 +0200

    TinyMCE editor: OK

--- resources/views/note/joint/new.blade.php
+++ resources/views/note/joint/new.blade.php
@@ -20,3 +20,29 @@ $note->findOrFail($noteId);
     <textarea name="text">Entrer une description</textarea>
 <input type="submit" name="" value="Enregistrer"/>
 </form>
+<!-- place in header of your html document -->
+<script>
+    tinymce.init({
+        selector: "textarea#text_editor",
+        theme: "modern",
+        width: 600,
+        height: 500,
+        plugins: [
+            "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+            "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+            "save table contextmenu directionality emoticons template paste textcolor"
+        ],
+
+        //content_css: "js/tinymce/css/content.css",
+        toolbar: "addFile insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+        style_formats: [
+            {title: 'Bold text', inline: 'b'},
+            {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+            {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+            {title: 'Example 1', inline: 'span', classes: 'example1'},
+            {title: 'Example 2', inline: 'span', classes: 'example2'},
+            {title: 'Table styles'},
+            {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+        ]
+    });
+</script>
