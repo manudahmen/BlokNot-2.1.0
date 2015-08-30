fdbd7f3d95a84c807f340d0df280b30adf713f32
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-30 11:47:08 +0200

    Search box

--- resources/views/navigation/navbuttons.blade.php
+++ resources/views/navigation/navbuttons.blade.php
@@ -5,3 +5,10 @@
  * Date: 26-08-15
  * Time: 03:01
  */
+?>
+<div id="searchBox">
+    <form action="search" method="get">
+        <input type="text" name="search" class="text-uppercase"/>
+        <input type="submit" name="submit" value="OK"/>
+    </form>
+</div>
