4c82bddbeffab5e23229572dc938536e05cc5eaa
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-08-30 11:42:56 +0200

    Modification de mise en page - I18N

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manue
+ * Date: 27-08-15
+ * Time: 17:02
+ */
+?>
+@section("sidebar")
+    @parent
+    <div class="miniImgContainerBottom">
+
+        <label>Actions</label>
+        <ul>
+            <li><a href="{{asset ("note/list/0/1")}}">Retour au dossier racine</a></li>
+        </ul>
+    </div>
+
+
+@stop
