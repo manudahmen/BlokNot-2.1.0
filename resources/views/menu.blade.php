b4f09d4243ba8bb51c23a561a5c1b9cdae4458be
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-09-26 09:50:10 +0200

    J'ai l'impression de faire du bon travail... simple mais bien!

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -9,8 +9,11 @@
 
 @section("sidebar")
     @parent
-    <li><a href="{{asset ("note/list/0/1")}}"><img src="/images/root.png"/><label class="onrolloverShow">Retour au
-                            dossier racine<</label>/a></li>
+    <li>
+        <a href="{{asset ("note/list/0/1")}}" title="Retour au dossier racine">
+            <div id="goto_root_folder"></div>
+        </a>
+    </li>
             <?php
             if(isset($noteId) && $noteId != 0)
             {
