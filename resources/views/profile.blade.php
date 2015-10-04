b684460108f63f271239b70939fe607177b2938c
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-04 10:18:41 +0200

    Typo

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,21 +10,30 @@
 @section('title', 'My settings')
 
 @section('content')
+    <h2>Editer les informations de profil</h2>
     @parent
     <form action="profile/save" method="POST">
     <table>
         <tr>
-            <td><label for="username">Username</label></td>
-            <td><input name="username" value="{{ Auth::user()->email }}"/></td>
+            <td><label for="password2">Entrez &agrave; nouveau votre mot de passe pour modifier</label></td>
+            <td><input class="required"name="password2" value=""/></td>
         </tr>
         <tr>
-            <td><label for="password">Password (<a href="{{asset("change-password")}}">change</a></label></td>
+            <td><label for="username">Nom d'utilisateur</label></td>
+            <td><input name="username" value="{{ Auth::user()->email }}"/></td>
+        </tr>0
+        <tr>
+            <td><label for="password">Mot de passe</label></td>
             <td><input name="password" value="{{ Auth::user()->email }}"/></td>
         </tr>
-        <tr><td><label for="fullname">Full Name</label></td>
+        <tr><td><label for="fullname">Nom complet</label></td>
         <td><input name="fullname" value="Not defined"/></td></tr>
         <tr><td><label for="rdio_username">rdio.com login</label></td>
             <td><input name="rdio_username" value="Not defined"/></td></tr>
+        <tr>
+            <td>Enregistrer</td>
+            <td><input type="submit" name="Envoyer" /></td>
+        </tr>
     </table>
     </form>
     @stop
