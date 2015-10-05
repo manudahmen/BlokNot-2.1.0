fb166ecfc31460fc0ba767479e1e4145e6ca9619
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-05 15:49:01 +0200

    Minor change

---
+++ app/Http/Controllers/ShareController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manue
+ * Date: 05-10-15
+ * Time: 15:47
+ */
+
+namespace App\Http\Controllers;
+
+
+class ShareController
+{
+
+}
--- app/Share.php
+++ app/Share.php
@@ -6,5 +6,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class Share extends Model
 {
-    //
+    private $table = "share";
+    private $timestamps = true;
+
+    private $id;
+    private $noteId;
+    private $user;
+    private $public = false;
+
+    public function __construct__($shareId, $noteId, $username, $public = false)
+    {
+        $this->id = $shareId;
+        $this->noteId = $noteId;
+        $this->username = $username;
+        $this->public = $public;
+        if($shareId!=NULL && $shareId!=0
+        )
+        {
+            $this->load($shareId);
+        }
+
+    }
+
+    public function load($shareId)
+    {
+        global $mysqli;
+        $this->shareId = $this->shareId = $shareId;
+        $row = getShareRow($shareId);
+        if ($row != FALSE)
+        {
+            $this->noteId = $row["note_id"];
+            $this->username = $row["username"];
+            $this->public = $row["public"];
+
+            $this->setAttribute("note_id", $row["note_id"]);
+            $this->setAttribute("username", $row["username"]);
+            $this->setAttribute("public", $row["public"]);
+
+        } else {
+            $this->id = 0;
+        }
+    }
+
+
+
+
 }
