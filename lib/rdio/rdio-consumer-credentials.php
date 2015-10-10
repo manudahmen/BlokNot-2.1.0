f329141e312770ed8c765dbb781d1bd6c399cd85
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-11 00:43:26 +0200

    add php lib for rdio

--- lib/rdio/rdio-consumer-credentials.php
+++ lib/rdio/rdio-consumer-credentials.php
@@ -1,5 +1,5 @@
 <?php
 # you can get these by signing up for a developer account at:
 # http://www.rdio.com/developers/
-define('RDIO_CONSUMER_KEY', '');
-define('RDIO_CONSUMER_SECRET', '');
+define('RDIO_CONSUMER_KEY', 'n24hkjfk7fc2xh6352bfxonn5q');
+define('RDIO_CONSUMER_SECRET', 'fvYGLIMVXyOP6UuKpBDZuQ');
