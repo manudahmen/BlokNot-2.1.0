b511e1d30444093f585c744c4fa8a5ee45a232de
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-12 13:16:46 +0200

    Add audio + rename root folder

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manue
+ * Date: 12-10-15
+ * Time: 13:01
+ */
+
+namespace App\Http\Controllers;
+
+
+
+class ProfileController extends Controller
+{
+    public function save(Request $request)
+    {
+
+        $currentPassword = Input::get('email');
+        $newPassword Input::get('password');
+        $newPasswordCoofirm = Input::get('password2');
+        $username = Input::get('email');
+        $fullName = Input::get('fullname');
+
+        $user = Auth()::user();
+
+        //if (Hash::check(Auth::user()->password, Hash::make(Input::get('currentPassword2'))))
+        //{
+            // The passwords match...
+            if (Auth::attempt(array('email' => $username, 'password' => $currentPassword)))
+            {
+
+                $user = Auth::user();
+
+                echo "Non enregistréé!!";
+            }
+        }
+    }
+}
