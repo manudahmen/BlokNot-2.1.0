2a1aa0823a2c121c9d1f2841428b012af2307578
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-20 04:34:29 +0200

    Stable but what's that Framework?

--- app/User.php
+++ app/User.php
@@ -2,17 +2,16 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContractimplements
-{
-
-}
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
     use Authenticatable, CanResetPassword;
 
-    //use RemindableTrait;
     /**
      * The database table used by the model.
      *
