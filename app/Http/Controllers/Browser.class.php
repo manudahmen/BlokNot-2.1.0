89c74f467ddf79eaaeaf289f33f9f9dcbc7833f2
Manuel Dahmen <manuel.dahmen@gmail.com>
2016-01-10 21:05:04 +0100

    Starting guest feature

---
+++ app/Http/Controllers/Browser.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Win
+ * Date: 10-01-16
+ * Time: 20:57
+ */
+
+namespace App\Http\Browser;
+
+
+class BrowserController
+{
+    /***
+     * @var int Affichage du navigateur de fichier à partir d'un fichier déterminé ou
+     * du fichier "racine". Recherche en base de données et sur disque.
+     */
+    public $MODE_NORMAL = 1;
+    /***
+     * @var int Affichage des fichiers partagés avec un utilisateur déterminé
+     */
+    public $MODE_GUEST = 2;
+    /***
+     * @var int Affichage en vue de sélection un fichier retour par JS ou Requête HTTP GET
+     * (modalités pas encore définie)
+     */
+    public $MODE_SELECTION = 4;
+    /***
+     * @var int Idem que $MODE_SELECTION mais fichiers multiples
+     */
+    public $MODE_SELECTION_MULTI = 8;
+
+    public function __construct($folderId = 0, $mode = 1)
+    {
+
+    }
+}
--- resources/views/guests/common_space.blade.php
+++ resources/views/guests/common_space.blade.php
@@ -7,6 +7,7 @@
  */
 ?>
 <p><input type="checkbox" name="autocopy"/>Copie automatique vers mes fichiers et protection</p>
-<h2>Liste des fichiers partagés de {{ $guest->hasOne('user')->hasOne('persona') }}</h2>
+<h2>Liste des fichiers partagés avec {{ $guest->hasOne('user')->hasOne('persona') }}</h2>
 
 <h2>Partager des fichiers avec</h2>
+<p><a href="{{ asset("guests/offer_place")  }}">Quelqu'un</a></p>
